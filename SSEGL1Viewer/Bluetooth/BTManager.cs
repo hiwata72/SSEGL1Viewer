0d79b25e4f8e31f8b3a993af58246ef0be52c13b
hiwat <hiwata72@hotmail.com>
2026-07-28 16:45:32 +0900

    Ver0.2.1 Windows BLE API参照確認

--- SSEGL1Viewer/Bluetooth/BTManager.cs
+++ SSEGL1Viewer/Bluetooth/BTManager.cs
@@ -1,23 +1,35 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
 
 namespace SSEGL1Viewer.Bluetooth
 {
     internal class BTManager
     {
+        // Windows BLE API が参照できるか確認するためのテスト
+        public void TestWindowsBleApi()
+        {
+            string selector = BluetoothLEDevice.GetDeviceSelector();
+
+            _ = selector;
+            _ = typeof(DeviceInformation);
+        }
+
+        // 仮実装（後で本物のBLE検索に置き換える）
         public async Task<List<BluetoothDeviceInfo>> ScanAsync()
         {
-            await Task.Delay(1000);
+            // WinRT API が参照できることを確認
+            TestWindowsBleApi();
 
-            return new List<BluetoothDeviceInfo>()
+            await Task.Delay(500);
+
+            return new List<BluetoothDeviceInfo>
             {
-                new BluetoothDeviceInfo()
+                new BluetoothDeviceInfo
                 {
-                    Name="SSE-GL1 (Sample)",
-                    Address="00:11:22:33:44:55"
+                    Name = "SSE-GL1 (Sample)",
+                    Address = "00:11:22:33:44:55"
                 }
             };
         }
-
     }
 }
